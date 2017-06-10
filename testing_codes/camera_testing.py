8d7a99e049b9a2add8017ed5228382ab49999b83
Samith Basnayaka <bmsd.basnayaka@gmail.com>
2017-06-10 23:59:29 +0530

    camera testing

---
+++ testing_codes/camera_testing.py
@@ -0,0 +1,9 @@
+import picamera
+
+#setup the camera
+print("Camera ready!")
+with picamera() as camera:
+    camera.resolution = (1280,720)
+    camera.capture("i/home/pi/Desktop/cookie/newimage.jpg")
+
+print("picture taken")

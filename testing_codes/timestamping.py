2f05cf3464371343bc928c69483e1c74798652bc
Samith Basnayaka <bmsd.basnayaka@gmail.com>
2017-06-13 01:03:45 +0530

    video testing

---
+++ testing_codes/timestamping.py
@@ -0,0 +1,15 @@
+import picamera
+
+file = "/home/pi/Desktop/cookie"
+timestamp_msg="Driver&time"
+
+print("Camera ready!")
+with picamera.PiCamera() as camera:
+    camera.resolution = (1280,720)
+    camera.capture(file)
+    print("picture taken")
+
+command = "/user/bin/convert" +file+ " -pointsize 32 \ -fill red -annotate +700+500 '" +timestamp_msg+ "' "+file
+
+#Execute our command
+call([command], shell=True)
